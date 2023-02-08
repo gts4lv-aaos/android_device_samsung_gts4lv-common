19394c86b4323820289f29f0076223d34d62ad50
Cosmin Tanislav <demonsingur@gmail.com>
2023-02-09 01:00:08 +0200

    gts4lv-common: use landscape orientation as default
    Change-Id: Iee725969fb2c66403fb69b57fd44f15d8210d25a

--- gts4lv.mk
+++ gts4lv.mk
@@ -29,8 +29,8 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 2560
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -144,8 +144,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.primary_display_orientation=ORIENTATION_270
+    ro.surface_flinger.max_virtual_display_dimension=4096
 
 # DRM
 PRODUCT_PACKAGES += \
